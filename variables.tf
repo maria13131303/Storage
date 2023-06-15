7b26387330fac947d3f80fb5fada1274d52ba90e
maria_13 <47932892+maria13131303@users.noreply.github.com>
2023-06-15 09:50:55 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "name" {
+  type = string
+  description = "Storage queue Name"
+}
+
+variable "storage_account_name" {
+  type = string
+  description = "Storge account that will host the storage queue"
+}
